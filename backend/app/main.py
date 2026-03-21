b1d5e712a22f53629d862175bbebf25811a51eb1
Abby <abby_bush42@gmail.com>
2026-03-20 21:58:36 -0500

    Initial backend setup

---
+++ backend/app/main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from .models import Entry
+from .models import EntryCreate
+from fastapi import Depends
+from sqlmodel import Session
+from .db import create_db_and_tables, get_session
+from typing import List
+from sqlmodel import select
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi import status
+
+
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hey girl. Mood Journal API is running"}
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+# @app.post("/entry")
+# def create_entry(entry: Entry, session: Session = Depends(get_session)):
+#     session.add(entry)
+#     session.commit()
+#     session.refresh(entry)
+#     return entry
+
+@app.post("/entry")
+def create_entry(entry: EntryCreate, session: Session = Depends(get_session)):
+    db_entry = Entry(**entry.dict())
+    session.add(db_entry)
+    session.commit()
+    session.refresh(db_entry)
+    return db_entry
+
+@app.get("/entries", response_model=List[Entry])
+def list_entries(session: Session = Depends(get_session)):
+    entries = session.exec(select(Entry)).all()
+    return entries
+
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"detail": exc.errors()},
+    )
---
+++ backend/app/models.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel, Field
+from datetime import date
+from typing import Optional
+
+class Entry(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    date: date
+    mood: int
+    note: Optional[str] = None
+
+class EntryCreate(SQLModel):
+    date: date
+    mood: int = Field(ge=1, le=5)
+    note: Optional[str] = None
